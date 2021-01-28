c75768c712e7ea8123f9ae1e026ce28279bcce76
H.R.Graf <hrgraf@gmx.ch>
2021-01-28 23:24:19 +0100

    compiled standalone

--- tools/jucer/Standalone/JuceLibraryCode/AppConfig.h
+++ tools/jucer/Standalone/JuceLibraryCode/AppConfig.h
@@ -36,14 +36,14 @@
 // BEGIN SECTION A
 
 #ifndef JUCE_DISPLAY_SPLASH_SCREEN
- #define JUCE_DISPLAY_SPLASH_SCREEN 0
+ #define JUCE_DISPLAY_SPLASH_SCREEN 1
 #endif
 
 // END SECTION A
 
 #define JUCE_USE_DARK_SPLASH_SCREEN 1
 
-#define JUCE_PROJUCER_VERSION 0x60004
+#define JUCE_PROJUCER_VERSION 0x60007
 
 //==============================================================================
 #define JUCE_MODULE_AVAILABLE_jlv2_host                  1
